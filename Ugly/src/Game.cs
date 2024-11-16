85ae2a9c14e77814f72cef32fefd98a223e5059d
samwizardry <samwizardry@gmail.com>
2024-11-16 20:51:17 +0300

    Hello Triangle

--- Ugly/src/Game.cs
+++ Ugly/src/Game.cs
@@ -7,12 +7,30 @@ namespace Ugly;
 
 internal class Game : GameWindow
 {
-    private readonly float[] _vertices =
+    private readonly float[] _triangle =
         [-0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f];
+          0.5f, -0.5f, 0.0f,
+          0.0f,  0.5f, 0.0f];
 
-    private readonly uint[] indices = [0, 1, 3];
+    private readonly float[] _leftTriangle =
+        [-0.9f, -0.5f, 0.0f,
+         -0.1f, -0.5f, 0.0f,
+         -0.5f,  0.5f, 0.0f];
+
+    private readonly float[] _rightTriangle =
+        [0.1f, -0.5f, 0.0f,
+         0.9f, -0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f];
+
+    private readonly float[] _rectangle =
+        [-0.5f, 0.5f, 0.0f,
+          0.5f, 0.5f, 0.0f,
+          0.5f, -0.5f, 0.0f,
+         -0.5f, -0.5f, 0.0f,];
+
+    private readonly uint[] _indices =
+        [0, 1, 2,
+         0, 3, 2];
 
     private readonly string _vertexShaderSource = """
         #version 330 core
@@ -34,11 +52,22 @@ internal class Game : GameWindow
         }
         """;
 
-    private int _vbo;
-    private int _vertexShader;
-    private int _fragmentShader;
+    private readonly string _fragmentShaderSourceRightTriangle = """
+        #version 330 core
+        out vec4 FragColor;
+
+        void main()
+        {
+            FragColor = vec4(0.2f, 0.5f, 1.0f, 1.0f);
+        }
+        """;
+
+    private int _vertexArrayLeftTriangle;
+    private int _vertexBufferLeftTriangle;
+    private int _vertexArrayRightTriangle;
+    private int _vertexBufferRightTriangle;
     private int _shaderProgram;
-    private int _vao;
+    private int _shaderProgramRightTriangle;
 
     public Game(int width, int height, string title)
         : base(
@@ -57,51 +86,142 @@ internal class Game : GameWindow
 
         GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-        _vbo = GL.GenBuffer();
-        GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
-        GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+        // Uncomment this call to draw in wireframe polygons.
+        //GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
 
-        _vao = GL.GenVertexArray();
-        GL.BindVertexArray(_vao);
+
+        // set up vertex data (and buffer(s)) and configure vertex attributes
+        // ------------------------------------------------------------------
+        _vertexArrayLeftTriangle = GL.GenVertexArray();
+        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+        GL.BindVertexArray(_vertexArrayLeftTriangle);
+
+        _vertexBufferLeftTriangle = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferLeftTriangle);
+        GL.BufferData(BufferTarget.ArrayBuffer, _leftTriangle.Length * sizeof(float), _leftTriangle, BufferUsageHint.StaticDraw);
 
         GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
         GL.EnableVertexAttribArray(0);
 
-        _vertexShader = GL.CreateShader(ShaderType.VertexShader);
-        GL.ShaderSource(_vertexShader, _vertexShaderSource);
-        GL.CompileShader(_vertexShader);
+        // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
 
-        _fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
-        GL.ShaderSource(_fragmentShader, _fragmentShaderSource);
-        GL.CompileShader(_fragmentShader);
+        // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+        //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
+        // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+        // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+        GL.BindVertexArray(0);
+
+
+        // set up right triangle
+        // ------------------------------------
+        _vertexArrayRightTriangle = GL.GenVertexArray();
+        GL.BindVertexArray(_vertexArrayRightTriangle);
+
+        _vertexBufferRightTriangle = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferRightTriangle);
+        GL.BufferData(BufferTarget.ArrayBuffer, _rightTriangle.Length * sizeof(float), _rightTriangle, BufferUsageHint.StaticDraw);
+
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+        GL.EnableVertexAttribArray(0);
+
+        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        GL.BindVertexArray(0);
+
+
+        // build and compile our shader program
+        // ------------------------------------
+        // vertex shader
+        int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+        GL.ShaderSource(vertexShader, _vertexShaderSource);
+        GL.CompileShader(vertexShader);
+
+        // check for shader compile errors
+        GL.GetShader(vertexShader, ShaderParameter.CompileStatus, out int success);
+        if (success != (int)All.True)
+        {
+            string infoLog = GL.GetShaderInfoLog(vertexShader);
+            throw new Exception($"An error occurred whilst compiling vertex shader '{vertexShader}'.\n{infoLog}");
+        }
+
+        // fragment shader
+        int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+        GL.ShaderSource(fragmentShader, _fragmentShaderSource);
+        GL.CompileShader(fragmentShader);
+
+        // check for shader compile errors
+        GL.GetShader(fragmentShader, ShaderParameter.CompileStatus, out success);
+        if (success != (int)All.True)
+        {
+            string infoLog = GL.GetShaderInfoLog(fragmentShader);
+            throw new Exception($"An error occurred whilst compiling fragment shader '{fragmentShader}'.\n{infoLog}");
+        }
+
+        // fragment shader right triangle
+        int fragmentShaderRightTriangle = GL.CreateShader(ShaderType.FragmentShader);
+        GL.ShaderSource(fragmentShaderRightTriangle, _fragmentShaderSourceRightTriangle);
+        GL.CompileShader(fragmentShaderRightTriangle);
+
+        // check for shader compile errors
+        GL.GetShader(fragmentShaderRightTriangle, ShaderParameter.CompileStatus, out success);
+        if (success != (int)All.True)
+        {
+            string infoLog = GL.GetShaderInfoLog(fragmentShaderRightTriangle);
+            throw new Exception($"An error occurred whilst compiling fragment shader '{fragmentShaderRightTriangle}'.\n{infoLog}");
+        }
+
+        // link shaders
         _shaderProgram = GL.CreateProgram();
-        GL.AttachShader(_shaderProgram, _vertexShader);
-        GL.AttachShader(_shaderProgram, _fragmentShader);
+        GL.AttachShader(_shaderProgram, vertexShader);
+        GL.AttachShader(_shaderProgram, fragmentShader);
         GL.LinkProgram(_shaderProgram);
 
-        GL.DeleteShader(_vertexShader);
-        GL.DeleteShader(_fragmentShader);
+        // check for linking errors
+        GL.GetProgram(_shaderProgram, GetProgramParameterName.LinkStatus, out success);
+        if (success != (int)All.True)
+        {
+            string infoLog = GL.GetProgramInfoLog(_shaderProgram);
+            throw new Exception($"An error occurred whilst linking  program '{_shaderProgram}'.\n{infoLog}");
+        }
+
+        _shaderProgramRightTriangle = GL.CreateProgram();
+        GL.AttachShader(_shaderProgramRightTriangle, vertexShader);
+        GL.AttachShader(_shaderProgramRightTriangle, fragmentShaderRightTriangle);
+        GL.LinkProgram(_shaderProgramRightTriangle);
+
+        // check for linking errors
+        GL.GetProgram(_shaderProgramRightTriangle, GetProgramParameterName.LinkStatus, out success);
+        if (success != (int)All.True)
+        {
+            string infoLog = GL.GetProgramInfoLog(_shaderProgramRightTriangle);
+            throw new Exception($"An error occurred whilst linking  program '{_shaderProgramRightTriangle}'.\n{infoLog}");
+        }
+
+        GL.DeleteShader(vertexShader);
+        GL.DeleteShader(fragmentShader);
+        GL.DeleteShader(fragmentShaderRightTriangle);
     }
 
     protected override void OnUnload()
     {
         base.OnUnload();
 
-        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-        GL.BindVertexArray(0);
-        GL.UseProgram(0);
+        // optional: de-allocate all resources once they've outlived their purpose:
+        // ------------------------------------------------------------------------
+        GL.DeleteVertexArray(_vertexArrayLeftTriangle);
+        GL.DeleteBuffer(_vertexBufferLeftTriangle);
 
-        GL.DeleteBuffer(_vbo);
-        GL.DeleteVertexArray(_vao);
+        GL.DeleteVertexArray(_vertexArrayRightTriangle);
+        GL.DeleteBuffer(_vertexBufferRightTriangle);
+
         GL.DeleteProgram(_shaderProgram);
+        GL.DeleteProgram(_shaderProgramRightTriangle);
     }
 
     protected override void OnUpdateFrame(FrameEventArgs args)
     {
         base.OnUpdateFrame(args);
-
-        Console.WriteLine(args.Time);
 
         if (KeyboardState.IsKeyDown(Keys.Escape))
         {
@@ -115,25 +235,28 @@ internal class Game : GameWindow
 
         GL.Clear(ClearBufferMask.ColorBufferBit);
 
-        GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
-        GL.BindVertexArray(_vao);
         GL.UseProgram(_shaderProgram);
 
+        GL.BindVertexArray(_vertexArrayLeftTriangle);
         GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
 
-        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-        GL.BindVertexArray(0);
-        GL.UseProgram(0);
+        GL.UseProgram(_shaderProgramRightTriangle);
 
+        GL.BindVertexArray(_vertexArrayRightTriangle);
+        GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
         SwapBuffers();
     }
 
+    // glfw: whenever the window size changed (by OS or user resize) this callback function executes
+    // ---------------------------------------------------------------------------------------------
     protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
     {
         base.OnFramebufferResize(e);
-
+        // make sure the viewport matches the new window dimensions; note that width and 
+        // height will be significantly larger than specified on retina displays.
         GL.Viewport(0, 0, e.Width, e.Height);
-
-        Console.WriteLine($"Resized: {e.Width}, {e.Height}");
     }
 }
